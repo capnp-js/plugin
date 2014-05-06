ebffaea24eec9740d10b59a70af96f932cd701e5
Tim Popham <popham@uw.edu>
2014-05-06 11:15:26 -0700

    Boolean decoding

--- src/template/data.js
+++ src/template/data.js
@@ -1,6 +1,5 @@
-var types = require('types');
-
 exports.byteOffset = function (chunk, ctx, bodies, params) {
+    var base = params.skip ? parseInt(params.skip) : 0;
     return (params.capnpSlot.type.bits >>> 3) * params.capnpSlot.offset;
 };
 
@@ -9,5 +8,10 @@ exports.byteCount = function (chunk, ctx, bodies, params) {
 };
 
 exports.boolMask = function (chunk, ctx, bodies, params) {
-    return 1 << (params.bitDistance % 8);
+    return (1 >>> 0) << (params.bitDistance & 0x00000007);
+};
+
+// Little endian least significant byte.
+exports.lsB = function (chunk, ctx, bodies, params) {
+    return 8*params.word;
 };
