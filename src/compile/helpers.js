fcc69dd1864d952dfeea4a6d5c404d5ea6c47992
Tim Popham <popham@uw.edu>
2014-05-07 16:46:30 -0700

    Template helpers

--- src/compile/helpers.js
+++ src/compile/helpers.js
@@ -1,17 +1,19 @@
-exports.byteOffset = function (chunk, ctx, bodies, params) {
+var dust = require('dustjs-linkedin');
+
+dust.helpers.byteOffset = function (chunk, ctx, bodies, params) {
     var base = params.skip ? parseInt(params.skip) : 0;
     return (params.capnpSlot.type.bits >>> 3) * params.capnpSlot.offset;
 };
 
-exports.byteCount = function (chunk, ctx, bodies, params) {
+dust.helpers.byteCount = function (chunk, ctx, bodies, params) {
     return params.capnpType.bits >>> 3;
 };
 
-exports.boolMask = function (chunk, ctx, bodies, params) {
+dust.helpers.boolMask = function (chunk, ctx, bodies, params) {
     return (1 >>> 0) << (params.bitDistance & 0x00000007);
 };
 
 // Little endian least significant byte.
-exports.lsB = function (chunk, ctx, bodies, params) {
+dust.helpers.lsB = function (chunk, ctx, bodies, params) {
     return 8*params.word;
 };
