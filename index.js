85372b9ac8e626a012c9eee8a524573817cd0bbf
Tim Popham <popham@uw.edu>
2014-05-10 13:29:53 -0700

    Exported interface includes a Struct blob

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+var node = require('when/node');
+var dust = require('dustjs-helpers');
 
+// Introduce helpers into the Dust global.
+require('./template/helpers');
+
+// Introduce reader and its partials into the Dust global.
+require('./precompile/reader');
+
+exports.Struct = require('./src/base/struct');
+exports.Struct.reader = function (schema) {
+    var base = dust.makeBase(schema);
+    return node.lift(dust.render)('struct/reader', base);
+};
