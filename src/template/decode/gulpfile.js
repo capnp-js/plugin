f0f19e7e0ec2632eecce62dc69b97d6ec398b78a
Tim Popham <popham@uw.edu>
2014-05-14 08:28:42 -0700

    Migrate to subpackage, and beautify rendered templates

--- src/template/decode/gulpfile.js
+++ src/template/decode/gulpfile.js
@@ -1,9 +1,16 @@
-var rendering = require('./rendering');
+var stream = require('stream');
 
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var _ = require('lodash-node');
 var when = require('when/node');
 
+var beautify = require('gulp-beautify');
+// .pipe(beautify()) after render for pretty-printed modules, but leave it off
+// and get a whitespace flag pulled into `dust-compile` for literate bit-
+// shiftery (dust.optimizers.format = function(ctx, node) { return node };).
+
+var buffer = require('gulp-buffer');
 var clean = require('gulp-clean');
 var compile = require('gulp-dust');
 var concat = require('gulp-concat');
@@ -13,10 +20,9 @@ var rename = require('gulp-rename');
 var render = require('gulp-dust-render');
 var phonyVinyl = require('vinyl-source-stream');
 
-var primitives = ['Void', 'Bool', 'UInt8', 'UInt16', 'UInt32', 'UInt64', 'Int8', 'Int16', 'Int32', 'Int64', 'Float32', 'Float64'];
+var rendering = require('./rendering');
 
-// See issue #238 for dustjs
-// dust.optimizers.format = function(ctx, node) { return node }; magic to keep newlines.
+var primitives = ['Void', 'Bool', 'UInt8', 'UInt16', 'UInt32', 'UInt64', 'Int8', 'Int16', 'Int32', 'Int64', 'Float32', 'Float64'];
 
 gulp.task('clean', function () {
     return gulp.src(['./build/**/*', './precompile/**/*'], {read : false})
@@ -38,11 +44,11 @@ gulp.task('jshint', function () {
 });
 
 gulp.task('precompiled', function () {
-    return gulp.src('./src/template/decode/**/*.dust')
+    return gulp.src('./**/*.dust')
         .pipe(rename({ extname : "" }))
         .pipe(compile())
         .pipe(concat('precompiled.js'))
-        .pipe(insert.prepend('var dust = require("dustjs-linkedin/lib/server");'))
+        .pipe(insert.prepend('var dust = require("dustjs-helpers");'))
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         .pipe(gulp.dest('build'));
@@ -53,19 +59,22 @@ function sstream(text) {
     s._read = function noop() {};
     s.push(text);
     s.push(null);
+
+    return s;
 }
 
 var listTasks = _.map(primitives, function (primitive) { return 'list_' + primitive; });
 _(primitives).forEach(function (primitive) {
     gulp.task('list_' + primitive, ['precompiled'], function () {
         return sstream(primitive)
-            .pipe(insert.wrap('"', '"'))
-            .pipe(insert.wrap('{type:', '}'))
             .pipe(phonyVinyl('List' + primitive))
+            .pipe(insert.wrap('"', '"'))
+            .pipe(insert.wrap('{"type":', '}'))
+            .pipe(buffer())
             .pipe(render(rendering.list.primitive))
-            .pipe(rename({ extname: "js" }))
-            .pipe(jshint())
-            .pipe(jshint.reporter('default'))
+              .on('error', gutil.log)
+            .pipe(beautify())
+            .pipe(rename({ extname: ".js" }))
             .pipe(gulp.dest('build/list'));
     });
 });
@@ -74,6 +83,7 @@ gulp.task('listStruct', ['precompiled'], function () {
     return sstream('')
         .pipe(phonyVinyl('ListStruct.js'))
         .pipe(render(rendering.list.Struct))
+        .pipe(beautify())
         .pipe(gulp.dest('build/list'));
 });
 listTasks.push('listStruct');
@@ -82,6 +92,7 @@ gulp.task('listData', ['precompiled'], function () {
     return sstream('')
         .pipe(phonyVinyl('ListData.js'))
         .pipe(render(rendering.list.Data))
+        .pipe(beautify())
         .pipe(gulp.dest('build/list'));
 });
 listTasks.push('listData');
@@ -90,6 +101,7 @@ gulp.task('listText', ['precompiled'], function () {
     return sstream('')
         .pipe(phonyVinyl('ListText.js'))
         .pipe(render(rendering.list.Text))
+        .pipe(beautify())
         .pipe(gulp.dest('build/list'));
 });
 listTasks.push('listText');
