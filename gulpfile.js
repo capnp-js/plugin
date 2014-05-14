30d73dc3754c7c79c7b3b0bf6f9322692516ca2b
Tim Popham <popham@uw.edu>
2014-05-14 12:52:40 -0700

    Completed migration to subpackage

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,34 @@
 var gulp = require('gulp');
-var sq = require('streamqueue');
 
-var compile = require('gulp-dust');
-var concat = require('gulp-concat');
-var insert = require('gulp-insert');
+var chug = require('gulp-chug');
+var clean = require('gulp-clean');
 var jshint = require('gulp-jshint');
-var rename = require('gulp-rename');
+
+var primitives = ['Void', 'Bool', 'Float32', 'Float64',
+                  'UInt8', 'UInt16', 'UInt32', 'UInt64',
+                   'Int8',  'Int16',  'Int32',  'Int64']
 
 gulp.task('watch', function () {
-    gulp.watch('./src/**/*.js', ['base']);
+    gulp.watch('./src/**/*.js');
     gulp.watch([
-        './src/template/decode/**/*.dust',
-        './src/template/helpers.js'
-    ], ['reader']);
-    gulp.watch([
-        './src/template/encode/**/*.dust',
-        './src/template/helpers.js'
-    ], ['builder']);
+         './src/template/decode/**/*',
+        '!./src/template/decode/build{,/**}'
+    ], ['decode']);
 });
 
-gulp.task('base', function () {
-    return gulp.src('./src/**/*.js')
+gulp.task('decode', ['buildDecode'], function () {
+    return gulp.src('./src/template/decode/build/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
+        .pipe(gulp.dest('./decode'));
 });
 
-gulp.task('reader', ['base'], function () {
-    // grep for "//" type comments and issue error
-    var stream = sq({ objectMode: true });
-    stream.queue(gulp.src('./src/template/decode/**/*.dust'));
-    stream.queue(gulp.src('./src/template/offset/*.dust', { base : './src/template' }));
-    return stream.done()
-        .pipe(rename({ extname: "" }))
-        .pipe(compile())
-        .pipe(concat('reader.js'))
-        .pipe(insert.prepend('var dust = require("dustjs-linkedin/lib/server");'))
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-        .pipe(gulp.dest('precompile'));
+gulp.task('buildDecode', function () {
+    return gulp.src('./src/template/decode/gulpfile.js', { read : false })
+        .pipe(chug({ tasks : ['build'] }))
 });
 
-gulp.task('builder', ['base'], function () {
-    // grep for "//" type comments and issue error
-    var stream = sq({ objectMode: true });
-    stream.queue(gulp.src('./src/template/encode/**/*.dust'));
-    stream.queue(gulp.src('./src/template/offset/*.dust', { base : './src/template' }));
-    return stream.done()
-        .pipe(rename({ extname: "" }))
-        .pipe(compile())
-        .pipe(concat('builder.js'))
-        .pipe(insert.prepend('var dust = require("dustjs-linkedin/lib/server");'))
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-        .pipe(gulp.dest('precompile'));
+gulp.task('clean', function () {
+    return gulp.src('./decode/**/*')
+        .pipe(clean());
 });
