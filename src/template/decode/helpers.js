d063126160e4ef9fcf99cd34328ea04f323c70e6
Tim Popham <popham@uw.edu>
2014-05-14 08:26:19 -0700

    Write chunks to fix build

--- src/template/decode/helpers.js
+++ src/template/decode/helpers.js
@@ -1,20 +1,23 @@
-var dust = require('dustjs-linkedin');
+var dust = require('dustjs-helpers');
 var traverse = require('traverse');
 
 dust.helpers.injectLists = function (chunk, ctx, bodies, params) {
     var nodes = params.nodes;
     var primitives = ['Void', 'Bool', 'UInt8', 'UInt16', 'UInt32', 'UInt64', 'Int8', 'Int16', 'Int32', 'Int64', 'Float32', 'Float64'];
     var actives = [];
-    for (p in primitives) {
-        var count = traverse(nodes).reduce(function count(acc, node) {
-            var isActive = node['type'] === 'List' && node['elementType'] === p;
-            return isActive ? acc+1 : acc;
-        }, 0);
+    primitives.forEach(function (p) {
+        var count = traverse(nodes).reduce(
+            function (acc, node) {
+                var isActive = node.type === 'List' && node.elementType === p;
+                return isActive ? acc+1 : acc;
+            },
+            0
+        );
 
         if (count > 0) {
-            actives.push(p)
+            actives.push(p);
         }
-    }
+    });
 
     return chunk.write(actives.map(function (active) {
         return "var List"+active+" = require('capnp-js/build/List"+active+"');";
@@ -26,7 +29,7 @@ dust.helpers.injectBuiltType = function (chunk, ctx, bodies, params) {
     var type = params.type;
 
     var count = traverse(nodes).reduce(function count(acc, node) {
-        return node['type'] === type ? acc+1 : acc;
+        return node.type === type ? acc+1 : acc;
     }, 0);
 
     if (count > 0) {
@@ -48,35 +51,32 @@ dust.helpers.byteOffset = function (chunk, ctx, bodies, params) {
         case 'Bool':
             if (skip) {
                 return chunk.write("((("+offset+") >>> 3) + "+skip+")");
-            } else {
-                return chunk.write("(("+offset+") >>> 3)");
             }
+            return chunk.write("(("+offset+") >>> 3)");
         case 'Int8':
             if (skip) {
                 return chunk.write("(("+offset+") + "+skip+")");
-            } else {
-                return chunk.write("("+offset+")");
             }
+            return chunk.write("("+offset+")");
         case 'Int16':
-            return expression(2, offset, skip);
+            return chunk.write(expression(2, offset, skip));
         case 'Int32':
-            return expression(4, offset, skip);
+            return chunk.write(expression(4, offset, skip));
         case 'Int64':
-            return expression(8, offset, skip);
+            return chunk.write(expression(8, offset, skip));
         case 'UInt8':
             if (skip) {
                 return chunk.write("(("+offset+") + "+skip+")");
-            } else {
-                return chunk.write("("+offset+")");
             }
+            return chunk.write("("+offset+")");
         case 'UInt16':
-            return expression(2, offset, skip);
+            return chunk.write(expression(2, offset, skip));
         case 'UInt32':
-            return expression(4, offset, skip);
+            return chunk.write(expression(4, offset, skip));
         case 'UInt64':
-            return expression(8, offset, skip);
+            return chunk.write(expression(8, offset, skip));
         default:
-            throw new Error('byteOffset of unmanaged type');
+            throw new TypeError('byteOffset of unmanaged type: ' + type);
         }
     } else {
         switch (type) {
@@ -101,7 +101,7 @@ dust.helpers.byteOffset = function (chunk, ctx, bodies, params) {
         case 'UInt64':
             return chunk.write(8*offset + skip);
         default:
-            throw new Error('byteOffset of unmanaged type');
+            throw new TypeError('byteOffset of unmanaged type: ' + type);
         }
     }
 
@@ -115,33 +115,9 @@ dust.helpers.byteOffset = function (chunk, ctx, bodies, params) {
 };
 
 dust.helpers.boolMask = function (chunk, ctx, bodies, params) {
-    return (1 >>> 0) << (params.bitDistance & 0x00000007);
+    return chunk.write((1 >>> 0) << (params.bitDistance & 0x00000007));
 };
 
-// Little endian least significant byte.
 dust.helpers.lsB = function (chunk, ctx, bodies, params) {
-    return 8*params.word;
+    return chunk.write(8*params.word);
 };
-
-dust.helpers.pointer = {};
-
-dust.helpers.pointer.STRUCT = function (chunk, ctx, bodies, params) {
-    return 0x00000000;
-};
-
-dust.helpers.pointer.LIST = function (chunk, ctx, bodies, params) {
-    return 0x00000001;
-};
-
-dust.helpers.pointer.INTER_SEGMENT = function (chunk, ctx, bodies, params) {
-    return 0x00000002;
-};
-
-dust.helpers.pointer.CAPABILITY = function (chunk, ctx, bodies, params) {
-    return 0x00000003;
-};
-
-dust.helpers.pointer.type = function (chunk, ctx, bodies, params) {
-    return params.segment[params.p] & 0x00000003;
-};
-
