abf55683d5ea51d154989c5a3b67eb52a8a34509
Tim Popham <popham@uw.edu>
2014-05-14 12:50:35 -0700

    Resolved circular dependency in gulpfile instead of codebase

--- src/template/decode/rendering.js
+++ src/template/decode/rendering.js
@@ -1,42 +1,23 @@
 var when = require('when/node');
 var dust = require('dustjs-helpers');
 
-// Introduce helpers into the Dust global.
+// Populate the Dust global.
+require('./build/precompiled');
 require('./helpers');
-
-function contextFree(name) {
-    return function () {
-        return when.lift(dust.render)(name, {});
-    };
-}
-
-function loadPartials() {
-    require('./build/precompiled');
-}
-
-exports.struct = {};
-exports.struct.reader = function (schema) {
-    loadPartials();
-    return when.lift(dust.render)('struct/reader', schema);
-};
 
 exports.list = {};
 exports.list.primitive = function (context) {
-    loadPartials();
     return when.lift(dust.render)('list/primitive', context);
 };
 
 exports.list.Struct = function (context) {
-    loadPartials();
     return when.lift(dust.render)('list/Struct', context);
 };
 
 exports.list.Data = function (context) {
-    loadPartials();
     return when.lift(dust.render)('list/Data', context);
 };
 
 exports.list.Text = function (context) {
-    loadPartials();
     return when.lift(dust.render)('list/Text', context);
 };
