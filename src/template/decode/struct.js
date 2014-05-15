6b5a5fed99c86eade5c48a1aa5e89ac31efec535
Tim Popham <popham@uw.edu>
2014-05-14 17:37:41 -0700

    Syntax fixes, and new paths

--- src/template/decode/struct.js
+++ src/template/decode/struct.js
@@ -2,9 +2,9 @@ var when = require('when/node');
 var dust = require('dustjs-helpers');
 
 // Populate the Dust global.
-require('./build/precompiled');
+require('./precompiled');
 require('./helpers');
 
-exports.struct = function (schema) {
+module.exports = function (schema) {
     return when.lift(dust.render)('struct/reader', schema);
 };
