b373bc48229807b3186fbc0b89b272acb05caed1
Tim Popham <popham@uw.edu>
2014-05-15 11:06:25 -0700

    Using inheritence for non-performance related list stuff

--- src/template/decode/base/list.js
+++ src/template/decode/base/list.js
@@ -1,1 +1,11 @@
-module.exports = function () {};
+module.exports = function List(segments, segment, begin, length) {
+    this.__segments = segments;
+    this.__segment = segment;
+    this.__begin = begin;
+    this.__length = length;
+};
+
+Object.defineProperty(List.prototype, 'length', {
+    get : function () { return this.__length; },
+    set : function (value) { throw new Error('Readonly'); }
+});
