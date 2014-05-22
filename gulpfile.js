8ea6fdd6fd82baeaa0adf39570ef9e8a5bf45f10
Tim Popham <popham@uw.edu>
2014-05-22 12:43:42 -0700

    Use npm for versioning

--- gulpfile.js
+++ gulpfile.js
@@ -45,29 +45,3 @@ gulp.task('clean', function () {
     return gulp.src('./decode/**/*')
         .pipe(clean());
 });
-
-/*
- * If username and password break flow, then look over at
- * `http://git-scm.com/docs/gitcredentials.html` to inject.
- */
-gulp.task('release', ['bump'], function () {
-  var pkg = require('./package.json');
-  var v = 'v' + pkg.version;
-  var message = 'Release ' + v;
-
-  git.tag(v, message, {}, function () {
-      git.push('origin', 'master', '--tags').end();
-  });
-});
-
-gulp.task('bump', ['incrementVersion'], function () {
-    return gulp.src('./package.json')
-        .pipe(git.add())
-        .pipe(git.commit('Version bump'));
-});
-
-gulp.task('incrementVersion', function () {
-    return gulp.src('./package.json')
-        .pipe(bump())
-        .pipe(gulp.dest('./'));
-});
