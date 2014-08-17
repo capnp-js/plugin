dd34881d4eb72a8954cb9e860b6b63869cd0d4f5
Tim Popham <popham@uw.edu>
2014-08-17 11:23:04 -0700

    Couple build process to new layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,42 @@
 var gulp = require('gulp');
 
 var chug = require('gulp-chug');
-var clean = require('gulp-clean');
+var clean = require('gulp-rimraf');
 var jshint = require('gulp-jshint');
 
 gulp.task('watch', function () {
     gulp.watch('./src/**/*.js');
     gulp.watch([
          './src/template/decode/**/*',
-        '!./src/template/decode/build{,/**}'
+        '!./src/template/decode/script{,/**}'
     ], ['decode']);
 });
 
-gulp.task('classes', function () {
-    return gulp.src('./src/base/**/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-        .pipe(gulp.dest('./lib/base'));
-});
+gulp.task('build', ['decode']);
 
-gulp.task('decode', ['buildDecode', 'classes'], function () {
-    return gulp.src([
-        './src/template/decode/build/**/*.js',
-        './src/decode/**/*.js'
-    ])
+gulp.task('decode', ['buildDecode'], function () {
+    return gulp.src('./src/decode/script/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         .pipe(gulp.dest('./lib/decode'));
 });
 
 gulp.task('buildDecode', function () {
-    return gulp.src('./src/template/decode/gulpfile.js', { read : false })
+    return gulp.src('./src/decode/gulpfile.js', { read : false })
         .pipe(chug({ tasks : ['build'] }))
 });
 
-gulp.task('ci', function () {
-    return gulp.src('./src/template/decode/gulpfile.js', { read : false })
-        .pipe(chug({ tasks : ['ci'] }));
-});
+//gulp.task('ci', function () {
+//    return gulp.src('./src/decode/gulpfile.js', { read : false })
+//        .pipe(chug({ tasks : ['ci'] }));
+//});
 
-gulp.task('clean', function () {
+gulp.task('clean', ['cleanDecode'], function () {
     return gulp.src('./lib/**/*')
         .pipe(clean());
+});
+
+gulp.task('cleanDecode', function () {
+    return gulp.src('./src/decode/gulpfile.js', { read : false })
+        .pipe(chug({ tasks : ['clean'] }))
 });
