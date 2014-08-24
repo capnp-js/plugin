62fbd914bea7ee36d17209f12e4f132b9b47fcd7
Tim Popham <popham@uw.edu>
2014-08-23 20:26:08 -0700

    Encode defaults for persistence in files

---
+++ context/toBase64.js
@@ -0,0 +1,42 @@
+define([], function() {
+
+    /*
+     * Lifted from MDN:
+     * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Base64_encoding_and_decoding.
+     */
+    var uint6ToB64 = function (nUint6) {
+        return nUint6 < 26 ?
+            nUint6 + 65
+            : nUint6 < 52 ?
+            nUint6 + 71
+            : nUint6 < 62 ?
+            nUint6 - 4
+            : nUint6 === 62 ?
+            43
+            : nUint6 === 63 ?
+            47
+            :
+            65;
+    };
+
+    return function (aBytes) {
+        var nMod3 = 2, sB64Enc = "";
+        for (var nLen = aBytes.length, nUint24 = 0, nIdx = 0; nIdx < nLen; nIdx++) {
+            nMod3 = nIdx % 3;
+            if (nIdx > 0 && (nIdx * 4 / 3) % 76 === 0) { sB64Enc += "\r\n"; }
+            nUint24 |= aBytes[nIdx] << (16 >>> nMod3 & 24);
+            if (nMod3 === 2 || aBytes.length - nIdx === 1) {
+                sB64Enc += String.fromCharCode(
+                    uint6ToB64(nUint24 >>> 18 & 63),
+                    uint6ToB64(nUint24 >>> 12 & 63),
+                    uint6ToB64(nUint24 >>> 6 & 63),
+                    uint6ToB64(nUint24 & 63)
+                );
+                nUint24 = 0;
+            }
+        }
+
+        return sB64Enc.substr(0, sB64Enc.length - 2 + nMod3) +
+            (nMod3 === 2 ? '' : nMod3 === 1 ? '=' : '==');
+    };
+});
