b4b6f25a3554425e758aba4b5857370f9324dd04
Tim Popham <popham@uw.edu>
2014-09-13 22:16:32 -0700

    Group rendering promises

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-exports.objectify = require('./lib/decode/objectify');
+exports.rTypes = require('./lib/reader/rTypes');
+exports.rScope = require('./lib/reader/rScope');
+exports.constants = require('./lib/reader/constants');
+exports.readers = require('./lib/reader/readers');
+
+exports.bTypes = require('./lib/builder/bTypes');
+exports.bScope = require('./lib/builder/bScope');
+exports.builders = require('./lib/builder/builders');
