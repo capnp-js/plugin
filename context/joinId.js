7dc9f9e00c76d88b2ef41e7b45c017cfeacfe9ce
Tim Popham <popham@uw.edu>
2014-09-29 15:38:30 -0700

    Use hexadecimal representation for ids

--- context/joinId.js
+++ context/joinId.js
@@ -1,11 +1,11 @@
 define([], function () {
 
-    var zeros = '0000000000';
+    var zeros = "00000000";
 
     return function (id) {
-        var hi = id[0].toString();
-        var lo = id[1].toString();
+        var hi = id[0].toString(16);
+        var lo = id[1].toString(16);
 
-        return hi + zeros.substring(0, 10-lo.length) + lo;
+        return "0x" + hi + zeros.substring(0, 8-lo.length) + lo;
     };
 });
