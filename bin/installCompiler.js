1ab23ce9592c936fc7209d2ab1b9d2cd373eb260
Tim Popham <popham@uw.edu>
2014-12-24 13:43:54 -0800

    Use standard library stuff only

--- bin/installCompiler.js
+++ bin/installCompiler.js
@@ -1,54 +1,72 @@
 #!/usr/bin/env node
 
-var when = require('when/node');
-var exec = when.lift(require('child_process').exec);
-var whereis = when.lift(require('whereis'));
+var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
 
 var base = process.cwd();
 
-whereis('capnp')
-    .then(function (out) {
-        return exec(out + " --version | awk '{print $4}'")
-            .then(function (version) {
-                // Verify that the compiler version is at 0.5 or greater.
-                if (parseFloat(version.slice(0,3)) >= 0.5) {
-                    console.log('Found a Capnproto compiler: ' + out);
-                    return out;
-                }
+var clone = function (done) {
+    console.log(' Cloning repository...');
+    var git = spawn('git', [
+        'clone',
+        '--branch', 'master',
+        '--depth', '1',
+        'https://github.com/kentonv/capnproto.git',
+        'compiler'
+    ]);
 
-                console.log('Installing a Capnproto compiler');
-                var cmd = [
-                    'rm -rf compiler',
-                    '&&',
-                    'git',
-                    'clone',
-                    '--branch master',
-                    '--depth 1',
-                    'https://github.com/kentonv/capnproto.git',
-                    'compiler'
-                ];
-
-                console.log(' Cloning repository');
-                return exec(cmd.join(' '))
-                    .then(function () {
-                        var cmd = [
-                            'mkdir compiler/build',
-                            'cd compiler/c++',
-                            'autoreconf -i',
-                            './configure --prefix='+base+'/compiler/build',
-                            'make -j4',
-                            'make install'
-                        ];
-
-                        console.log(' Building');
-                        return exec(cmd.join('&&'));
-                    })
-                    .then(function () {
-                        return '../compiler/build/bin/capnp'
-                    });
-            });
-    })
-    .then(function (bin) {
-        console.log('Linking Capnproto compiler to local: ' + bin);
-        return exec('cd bin && rm -f capnp && ln -s ' + bin);
+    var errOut = '';
+    git.stderr.on('data', function (err) { errOut += err; });
+    git.on('close', function (code) {
+        if (code !== 0) throw new Error(errOut);
+        done();
     });
+};
+
+var build = function (done) {
+    var cmd = [
+        'mkdir compiler/build',
+        'cd compiler/c++',
+        'autoreconf -i',
+        './configure --prefix='+base+'/compiler/build',
+        'make -j4',
+        'make install',
+        'cd ../../bin',
+        'rm -f capnp',
+        'ln -s ../compiler/build/bin/capnp'
+    ];
+
+    console.log(' Building...');
+    exec(cmd.join(' && '), function (error, stdout, stderr) {
+        if (error) throw new Error(error);
+
+        console.log(' Linked to local Capnproto compiler');
+        done();
+    });
+};
+
+var version = spawn('capnp', ['--version']); // Finds prior local installs
+var awk = spawn('awk', ['{print $4}']);
+
+version.stderr.on('data', function (err) { throw new Error(err); });
+awk.stderr.on('data', function (err) { throw new Error(err); });
+
+version.stdout.on('data', function (data) { awk.stdin.write(data); });
+version.on('close', function () { awk.stdin.end(); });
+
+var n = '';
+awk.stdout.on('data', function (data) { n += data; });
+awk.on('close', function () {
+    if (parseFloat(n.slice(0,3)) >= 0.5) {
+        console.log('Found a Capnproto compiler');
+    } else {
+        console.log('No Capnproto compiler found.  Installing:');
+
+        var rm = spawn('rm', ['-rf', 'compiler']);
+        rm.on('close', function () {
+            clone(function () {
+                build(function () {});
+            });
+        });
+    }
+});
